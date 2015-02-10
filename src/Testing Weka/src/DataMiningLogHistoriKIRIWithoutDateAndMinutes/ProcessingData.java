d01c213ba07f8edc21bb62b8aab3ce37e28c216b
jovangunawan <jovan_gunawan1@yahoo.com>
2015-02-11 00:57:14 +0700

    - membuat package yang baru dimana atribut tanggal dan menit dibuang - GUI pada package yang baru sudah dapat digunakan - MVC sudah diimplementasikan - pemilihan id3 atau j48 sudah diimplementasikan

---
+++ src/Testing Weka/src/DataMiningLogHistoriKIRIWithoutDateAndMinutes/ProcessingData.java
@@ -0,0 +1,146 @@
+package DataMiningLogHistoriKIRIWithoutDateAndMinutes;
+
+import java.util.ArrayList;
+
+/*
+ * Class for processing data ONLY FOR DATA LOG HISTORY KIRI
+ */
+
+/**
+ *
+ * @author Jovan Gunawan
+ */
+public class ProcessingData
+{
+    ProcessingData()
+    {
+    }
+
+    public void processSorting(ArrayList addApiKey, ArrayList findRoute, ArrayList login, ArrayList nearbyTransport, ArrayList pageLoad, ArrayList register, ArrayList searchPlace, ArrayList widgetError, ArrayList widgetLoad, ArrayList data)
+    {
+        System.out.println("Banyak Data: " + data.size());
+        for(int i =0; i < data.size(); i++)
+        {
+            String [] tempData = (String[])data.get(i);
+            if(tempData[3].equals("ADDAPIKEY"))
+            {
+                addApiKey.add(tempData);
+            }
+            else if(tempData[3].equals("FINDROUTE"))
+            {
+                findRoute.add(tempData);
+            }
+            else if(tempData[3].equals("LOGIN"))
+            {
+                login.add(tempData);
+            }
+            else if(tempData[3].equals("NEARBYTRANSPORT"))
+            {
+                nearbyTransport.add(tempData);
+            }
+            else if(tempData[3].equals("PAGELOAD"))
+            {
+                pageLoad.add(tempData);
+            }
+            else if(tempData[3].equals("REGISTER"))
+            {
+                register.add(tempData);
+            }
+            else if(tempData[3].equals("SEARCHPLACE"))
+            {
+                searchPlace.add(tempData);
+            }
+            else if(tempData[3].equals("WIDGETERROR"))
+            {
+                widgetError.add(tempData);
+            }
+            else if(tempData[3].equals("WIDGETLOAD"))
+            {
+                widgetLoad.add(tempData);
+            }
+        }
+    }
+    public void processSorting(ArrayList array, ArrayList data, String action)
+    {
+        for(int i =0; i < data.size(); i++)
+        {
+            String [] tempData = (String[])data.get(i);
+            if(tempData[3].equals(action))
+            {
+                array.add(tempData);
+            }
+        }
+    }
+    public ArrayList preprocessingData(ArrayList<String[]> data)
+    {
+        ArrayList<int[]> result = new ArrayList<int[]>();
+        
+        // tahap pertama: ubah waktu dari UTC ke GMT+7
+        for(int i = 0; i < data.size(); i++)
+        {
+            //System.out.println(data.get(i)[3]);
+            data.get(i)[2] = TimezoneConverter.convertToGMT7(data.get(i)[2]);
+        }
+        
+        // tahap kedua sampai kesembilan
+        DistanceHaversine haversine = new DistanceHaversine();
+        for(int i = 0; i < data.size(); i++)
+        {
+            // tahap kedua: pecah string atribut tanggal
+            int[] temp = new int[5];
+            String[] splited = data.get(i)[2].split(" ");
+            temp[3] = Integer.parseInt(splited[0]);
+            // tahap ketiga: pecah nilai string yang tanggal
+            String[] splited2 = splited[1].split("/");
+            temp[0] = Integer.parseInt(splited2[0]);
+            temp[1] = Integer.parseInt(splited2[2]);
+            // tahap keempat: pecah nilai string yang jam
+            splited2 = splited[2].split(":");
+            temp[2] = Integer.parseInt(splited2[0]);
+            temp[3] = Integer.parseInt(splited2[1]);
+            // tahap kelima: pecah string atribut additional data
+            splited = data.get(i)[4].split("/");
+            // tahap keenam: pecah lokasi keberangkatan dan lokasi tujuan untuk mendapatkan lat n lon dan (ini tahap ketujuh) menghitung jarak terhadap titik pusat
+            splited2 = splited[0].split(",");
+            double jarakKeberangkatan = haversine.calculateDistance(Double.parseDouble(splited2[0]), Double.parseDouble(splited2[1]), -6.92036, 107.67023) * 1000;
+            splited2 = splited[1].split(",");
+            double jarakTujuan = haversine.calculateDistance(Double.parseDouble(splited2[0]), Double.parseDouble(splited2[1]), -6.92036, 107.67023) * 1000;
+            // tahap kedelapan, set semua data ke array
+            temp[4] = klasifikasiKelas(jarakKeberangkatan, jarakTujuan);
+            result.add(temp);
+        }
+        return result;
+    }
+    
+    public int klasifikasiKelas(double jarakKeberangkatan, double jarakTujuan)
+    {
+        int regionKeberangkatan, regionTujuan;
+        int klasifikasi = 0; // 0 --> tidak menuju Bandung, 1 --> menuju Bandung, 2 --> menuju region yang sama
+        
+        regionKeberangkatan = (int)jarakKeberangkatan;
+        regionTujuan = (int)jarakTujuan;
+        
+        if(regionKeberangkatan > 11)
+        {
+            regionKeberangkatan = 11;
+        }
+        if(regionTujuan > 11)
+        {
+            regionTujuan = 11;
+        }
+        
+        if (regionKeberangkatan > regionTujuan)
+        {
+            klasifikasi = -1;
+        }
+        else if (regionKeberangkatan < regionTujuan)
+        {
+            klasifikasi = 1;
+        }
+        else
+        {
+            klasifikasi = 0;
+        }
+        return klasifikasi;
+    }
+}
---
+++ src/Testing Weka/src/DataMiningLogHistoriKIRIWithoutDateAndMinutes/TimezoneConverter.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DataMiningLogHistoriKIRIWithoutDateAndMinutes;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jovan Gunawan
+ */
+public class TimezoneConverter 
+{
+    // Mengubah input waktu menjadi waktu GMT+7
+    // Jika ingin mengubah dari UTC ke GMT+7 maka komputer harus set timezone ke UTC
+    // input parameter string harus dalam format dd-MM-yyyy HH:mm:ss --> contoh 1/1/2014  3:51:15 AM
+    // output akan mengubah waktu menjadi GMT+8 dalam String dengan format EEE MM/dd/yyyy HH:mm:ss --> contoh Wed 01/01/2014 03:51:15
+    public static String convertToGMT7(String date)
+    {
+        Date d;
+        long milliseconds = 0;
+        
+        try {
+            SimpleDateFormat f = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+            d = (Date)f.parse(date);
+            milliseconds = d.getTime();
+        } catch (ParseException ex) {
+            Logger.getLogger(TimezoneConverter.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(1);
+        }
+        
+        Date currentTime = new Date(milliseconds);
+        // untuk hari --> 1 = senin,..., 7 = minggu
+        SimpleDateFormat sdf = new SimpleDateFormat("u MM/dd/yyyy HH:mm:ss");
+        
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT+7"));
+        return sdf.format(currentTime);
+    }
+}
